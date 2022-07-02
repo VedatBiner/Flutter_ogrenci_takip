56f3b58fb8a3e48cb3cdedbdafbb59acaf2d1ba0
Vedat Biner <vbiner@gmail.com>
2022-07-02 16:40:44 +0300

    Listview.builder eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,57 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
-  runApp(MaterialApp(
-    home: MyApp()
-  ));
-
+  runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   String mesaj = "İlk Uygulama";
   MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    var ogrenciler =["Vedat Biner", "Mehmet Biner", "Zeynep Biner", "Sevim Biner"];
     return Scaffold(
       appBar: AppBar(
         title: Text(mesaj),
       ),
-      body: Center(
-          child: ElevatedButton(
-            child: const Text("Sonucu Gör"),
-            onPressed: (){
-              int puan = 15;
-              String mesaj = "";
-              if (puan >= 50){
-                mesaj = "Geçti";
-              } else if (puan >= 40){
-                mesaj = "Bütünlemeye kaldı";
-              } else {
-                mesaj = "Kaldı";
-              }
-              var alert = AlertDialog(
-                title: const Text("Sınav Sonucu"),
-                content: Text(mesaj),
-              );
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) => alert);
-            }
-          )
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+      // Builder eleman sayısı kadar listeleme yapacak
+              itemCount: ogrenciler.length,
+              itemBuilder: (BuildContext context, int index){
+                return Text(ogrenciler[index]);
+              })
+            ),
+          Center(
+              child: ElevatedButton(
+                  child: const Text("Sonucu Gör"),
+                  onPressed: () {
+                    int puan = 15;
+                    String mesaj = "";
+                    if (puan >= 50) {
+                      mesaj = "Geçti";
+                    } else if (puan >= 40) {
+                      mesaj = "Bütünlemeye kaldı";
+                    } else {
+                      mesaj = "Kaldı";
+                    }
+                    var alert = AlertDialog(
+                      title: const Text("Sınav Sonucu"),
+                      content: Text(mesaj),
+                    );
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) => alert);
+                  })),
+        ],
       ),
     );
   }
 }
+
+
+
+
+
