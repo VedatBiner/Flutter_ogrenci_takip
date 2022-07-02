a435c2ec9890166dc9546c490773d3aed2f655cd
Vedat Biner <vbiner@gmail.com>
2022-07-02 17:06:38 +0300

    Sonuç hesaplama fonksiyonu eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,12 @@ class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    var ogrenciler =["Vedat Biner", "Mehmet Biner", "Zeynep Biner", "Sevim Biner"];
+    var ogrenciler = [
+      "Vedat Biner",
+      "Mehmet Biner",
+      "Zeynep Biner",
+      "Sevim Biner"
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text(mesaj),
@@ -17,39 +22,56 @@ class MyApp extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-      // Builder eleman sayısı kadar listeleme yapacak
-              itemCount: ogrenciler.length,
-              itemBuilder: (BuildContext context, int index){
-                return Text(ogrenciler[index]);
-              })
-            ),
+              child: ListView.builder(
+                  // Builder eleman sayısı kadar listeleme yapacak
+                  itemCount: ogrenciler.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Text(ogrenciler[index]);
+                  })),
           Center(
               child: ElevatedButton(
                   child: const Text("Sonucu Gör"),
                   onPressed: () {
-                    int puan = 15;
-                    String mesaj = "";
-                    if (puan >= 50) {
-                      mesaj = "Geçti";
-                    } else if (puan >= 40) {
-                      mesaj = "Bütünlemeye kaldı";
-                    } else {
-                      mesaj = "Kaldı";
-                    }
-                    var alert = AlertDialog(
-                      title: const Text("Sınav Sonucu"),
-                      content: Text(mesaj),
-                    );
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) => alert);
+                    var mesaj = sinavHesapla(55);
+                    mesajGoster(context, mesaj);
                   })),
         ],
       ),
     );
   }
+
+  String sinavHesapla(int puan) {
+    String mesaj = "";
+    if (puan >= 50) {
+      mesaj = "Geçti";
+    } else if (puan >= 40) {
+      mesaj = "Bütünlemeye kaldı";
+    } else {
+      mesaj = "Kaldı";
+    }
+    return mesaj;
+  }
+
+  void mesajGoster(BuildContext context, String mesaj) {
+    var alert = AlertDialog(
+      title: const Text("Sınav Sonucu"),
+      content: Text(mesaj),
+    );
+    showDialog(context: context, builder: (BuildContext context) => alert);
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
